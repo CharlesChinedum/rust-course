48787f5ad786f61e7a017962e6cd37a46bda1eac
charleschinedum <charleschinedum2@gmail.com>
2025-02-26 09:28:14 +0100

    add Media enum and implement print_media function; update main to demonstrate media types

--- media/src/main.rs
+++ media/src/main.rs
@@ -1,3 +1,30 @@
+#[derive(Debug)]
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, director: String },
+    Audiobook { title: String },
+}
+
+fn print_media(media: Media) {
+    println!("{:#?}", media);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let audiobook = Media::Audiobook {
+        title: String::from("An Audiobook"),
+    };
+
+    let good_movie = Media::Movie {
+        title: String::from("Good Movie"),
+        director: String::from("Good Direcotr"),
+    };
+
+    let bad_book = Media::Book {
+        title: String::from("Bad Book"),
+        author: String::from("Bad Author"),
+    };
+
+    println!("{:#?}", good_movie);
+    println!("{:#?}", bad_book);
+    println!("{:#?}", audiobook);
 }
