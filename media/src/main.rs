2b2b989b1003fc7555d5335bd572e7d3c06fa942
charleschinedum <charleschinedum2@gmail.com>
2025-03-01 00:06:33 +0100

    implement description method for Media enum; update main to display media descriptions

--- media/src/main.rs
+++ media/src/main.rs
@@ -5,6 +5,16 @@ enum Media {
     Audiobook { title: String },
 }
 
+impl Media {
+    fn description(&self) -> String {
+        match self {
+            Media::Book { title, author } => format!("{} by {}", title, author),
+            Media::Movie { title, director } => format!("{} by {}", title, director),
+            Media::Audiobook { title } => format!("{}", title),
+        }
+    }
+}
+
 fn print_media(media: Media) {
     println!("{:#?}", media);
 }
@@ -16,7 +26,7 @@ fn main() {
 
     let good_movie = Media::Movie {
         title: String::from("Good Movie"),
-        director: String::from("Good Direcotr"),
+        director: String::from("Good Director"),
     };
 
     let bad_book = Media::Book {
@@ -24,7 +34,11 @@ fn main() {
         author: String::from("Bad Author"),
     };
 
-    println!("{:#?}", good_movie);
-    println!("{:#?}", bad_book);
-    println!("{:#?}", audiobook);
+    println!("{}", audiobook.description());
+    println!("{}", good_movie.description());
+    println!("{}", bad_book.description());
+
+    // println!("{:#?}", good_movie);
+    // println!("{:#?}", bad_book);
+    // println!("{:#?}", audiobook);
 }
