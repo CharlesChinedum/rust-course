97d26bf4fb3b26ac34a5e835e2183f2dd278d3ed
charleschinedum <charleschinedum2@gmail.com>
2025-04-15 23:43:59 +0100

    update main to comment out log reading and add division example with error handling

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -2,9 +2,18 @@ use std::fs;
 use std::io::Error;
 
 fn main() {
-    let text = fs::read_to_string("logs.txt");
+    // let text = fs::read_to_string("logs.txt");
 
-    println!("{:#?}", text);
+    // println!("{:#?}", text);
+
+    match divide(5.0, 0.0) {
+        Ok(reult) => {
+            println!("Result: {}", reult);
+        }
+        Err(error) => {
+            println!("Error: {}", error);
+        }
+    }
 }
 
 fn divide(a: f64, b: f64) -> Result<f64, Error> {
