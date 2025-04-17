c195ebdfd26dfedb173305e0418e59d24e8d3497
charleschinedum <charleschinedum2@gmail.com>
2025-04-17 23:44:35 +0100

    add string_test function to demonstrate string handling in main

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,6 +1,16 @@
 use std::fs;
 
+fn string_test(a: String, b: &String, c: &str) {
+    println!("a: {}, b: {}, c: {}", a, b, c);
+}
+
 fn main() {
+    string_test(
+        "red".to_string(),
+        &String::from("green"),
+        String::from("blue").as_str(),
+    );
+
     match fs::read_to_string("logs.txt") {
         Ok(text) => {
             println!("File content: {}", text.len());
