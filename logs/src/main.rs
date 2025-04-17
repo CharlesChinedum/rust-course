89dc7341be7be639a55eb93179dcfed4446b4ca1
charleschinedum <charleschinedum2@gmail.com>
2025-04-17 22:46:13 +0100

    refactor main to comment out unused code and improve file reading error messages

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,43 +1,50 @@
 use std::fs;
-use std::io::Error;
+// use std::io::Error;
 
 fn main() {
     // let text = fs::read_to_string("logs.txt");
-
-    // println!("{:#?}", text);
-
-    match divide(5.0, 3.0) {
-        Ok(reult) => {
-            println!("Result: {}", reult);
+    match fs::read_to_string("logs.txt") {
+        Ok(text) => {
+            println!("File content: {}", text.len());
         }
-        Err(error) => {
-            println!("Error: {}", error);
-        }
-    }
-
-    let email = String::from("test@test.com");
-
-    match validate_email(email) {
-        Ok(..) => println!("Valid email"),
 
         Err(error) => {
-            println!("Error: {}", error)
+            println!("Failed to read: {}", error);
         }
     }
+
+    // match divide(5.0, 3.0) {
+    //     Ok(reult) => {
+    //         println!("Result: {}", reult);
+    //     }
+    //     Err(error) => {
+    //         println!("Error: {}", error);
+    //     }
+    // }
+
+    // let email = String::from("test@test.com");
+
+    // match validate_email(email) {
+    //     Ok(..) => println!("Valid email"),
+
+    //     Err(error) => {
+    //         println!("Error: {}", error)
+    //     }
+    // }
 }
 
-fn validate_email(email: String) -> Result<(), Error> {
-    if email.contains("@") {
-        Ok(())
-    } else {
-        Err(Error::other("Invalid email address"))
-    }
-}
+// fn validate_email(email: String) -> Result<(), Error> {
+//     if email.contains("@") {
+//         Ok(())
+//     } else {
+//         Err(Error::other("Invalid email address"))
+//     }
+// }
 
-fn divide(a: f64, b: f64) -> Result<f64, Error> {
-    if b == 0.0 {
-        Err(Error::other("Division by zero"))
-    } else {
-        Ok(a / b)
-    }
-}
+// fn divide(a: f64, b: f64) -> Result<f64, Error> {
+//     if b == 0.0 {
+//         Err(Error::other("Division by zero"))
+//     } else {
+//         Ok(a / b)
+//     }
+// }
