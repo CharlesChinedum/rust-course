ba054002961f81ae9790beb2f5d9c9f52827e43b
charleschinedum <charleschinedum2@gmail.com>
2025-04-17 22:48:54 +0100

    refactor main to remove commented-out code and streamline file reading logic

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,8 +1,6 @@
 use std::fs;
-// use std::io::Error;
 
 fn main() {
-    // let text = fs::read_to_string("logs.txt");
     match fs::read_to_string("logs.txt") {
         Ok(text) => {
             println!("File content: {}", text.len());
@@ -12,39 +10,4 @@ fn main() {
             println!("Failed to read: {}", error);
         }
     }
-
-    // match divide(5.0, 3.0) {
-    //     Ok(reult) => {
-    //         println!("Result: {}", reult);
-    //     }
-    //     Err(error) => {
-    //         println!("Error: {}", error);
-    //     }
-    // }
-
-    // let email = String::from("test@test.com");
-
-    // match validate_email(email) {
-    //     Ok(..) => println!("Valid email"),
-
-    //     Err(error) => {
-    //         println!("Error: {}", error)
-    //     }
-    // }
 }
-
-// fn validate_email(email: String) -> Result<(), Error> {
-//     if email.contains("@") {
-//         Ok(())
-//     } else {
-//         Err(Error::other("Invalid email address"))
-//     }
-// }
-
-// fn divide(a: f64, b: f64) -> Result<f64, Error> {
-//     if b == 0.0 {
-//         Err(Error::other("Division by zero"))
-//     } else {
-//         Ok(a / b)
-//     }
-// }
