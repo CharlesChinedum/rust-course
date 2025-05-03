af7ef3ba1ff346f3a54a722ec76abddb36a018f0
charleschinedum <charleschinedum2@gmail.com>
2025-05-03 22:41:50 +0100

    implement next_langauge function to find the next programming language in a list

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,27 @@
+fn next_langauge<'a>(languages: &'a [String], current: &str) -> &'a str {
+    let mut found = false;
+
+    for lang in languages {
+        if found {
+            return lang;
+        }
+
+        if lang == current {
+            found = true
+        }
+    }
+
+    languages.last().unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let languages = vec![
+        String::from("rust"),
+        String::from("go"),
+        String::from("typescript"),
+    ];
+
+    let result = next_langauge(&languages, "go");
+
+    println!("{}", result);
 }
